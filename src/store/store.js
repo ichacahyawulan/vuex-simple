03ef65e3111e157536eddd99325eb46906965354
Icha Cahya Wulan <ichacahyawulan@gmail.com>
2021-12-23 18:31:26 +0700

    Add files via upload

---
+++ src/store/store.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        isLoading: false, //CODE BERARU
+        listAgenda: [
+            { hari: 'senin', kegiatan: 'Belajar Vuejs' },
+            { hari: 'selasa', kegiatan: 'Belajar Laravel' },
+            { hari: 'rabu', kegiatan: 'Belajar Mysql' }
+        ]
+    },
+    mutations: {
+        KONFIRMASI_AGENDA: (state, agenda) => {
+            state.listAgenda.push(agenda)
+        }
+    },
+    actions: {
+        simpanAgenda({ commit, state }, agenda) {
+            state.isLoading = true
+            setTimeout(() => {
+                commit('KONFIRMASI_AGENDA', agenda)
+                state.isLoading = false
+            }, 1500)
+        }
+    }
+})
